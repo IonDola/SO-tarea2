b7f3f6a73073d8bb0a2636de21c76319e4e9ae29
IonDola <idolanescu@estudiantec.cr>
2025-09-09 11:08:51 -0600

    Mi compu habla jaónes y yo estoy calvo

--- operativeless-morse-conversor/src/booter.asm
+++ operativeless-morse-conversor/src/booter.asm
@@ -1,29 +1,17 @@
 bits 64
+default rel
+
+section .data
+    ; cadenas UTF-16 terminadas en 0
+    strInp db __utf16__ 'Escribe algo (ENTER para terminar):',0
+    strEnd dw 'T','e','r','m','i','n','a','m','o','s',0
+datasize equ $ - $$
+section .bss
+    inp    resw 24        ; buffer de 24 caracteres UTF-16
+    keyBuf resw 2         ; EFI_INPUT_KEY (ScanCode + UnicodeChar)
+
 section .text
 global _start
-struc EFI_TABLE_HEADER
-    .Signature RESQ 1
-    .Revision RESD 1
-    .HeaderSize RESD 1
-    .CRC32 RESD 1
-    .Reserved RESD 1
-endstruc
-
-struc EFI_SYSTEM_TABLE
-    .Hdr                  RESB EFI_TABLE_HEADER_size
-    .FirmwareVendor       RESQ 1
-    .FirmwareRevision     RESD 1
-    .ConsoleInHandle      RESQ 1
-    .ConIn                RESQ 1
-    .ConsoleOutHandle     RESQ 1
-    .ConOut               RESQ 1
-    .StandardErrorHandle  RESQ 1
-    .StdErr               RESQ 1
-    .RuntimeServices      RESQ 1
-    .BootServices         RESQ 1
-    .NumberOfTableEntries RESQ 1
-    .ConfigurationTable   RESQ 1
-endstruc
 
 struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
     .Reset             RESQ 1
@@ -38,28 +26,66 @@ struc EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
     .Mode              RESQ 1
 endstruc
 
-loopForever:
-    jmp loopForever
+struc EFI_SIMPLE_TEXT_INPUT_PROTOCOL
+    .Reset         RESQ 1
+    .ReadKeyStroke RESQ 1
+    .WaitForKey    RESQ 1
+endstruc
 
 _start:
-    sub rsp, 4 * 8
-    ; rdx points to the EFI_SYSTEM_TABLE structure
-    ; which is the 2nd argument passed to us by the UEFI firmware
-    ; adding 64 causes rcx to point to EFI_SYSTEM_TABLE.ConOut
-    mov rcx, [rdx + 64]
+    ; alinear y reservar shadow space
+    mov rax, rsp
+    and rsp, -16
+    sub rsp, 32
 
-    lea rdx, [rel strHello]
+    ; --------------------------
+    ; mostrar mensaje inicial
+    ; rdx = EFI_SYSTEM_TABLE
+    mov rdi, rdx
 
+    mov rcx, [rdi + 0x40]         ; ConOut (offset 0x40)
+    lea rdx, [strInp]             ; puntero a cadena UTF-16
     call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
 
-    jmp loopForever
+    ; --------------------------
+    ; leer teclado
+    mov rcx, [rdi + 0x30]            ; ConIn (offset 0x30)
+    lea rsi, [inp]                ; buffer de entrada
+    xor rbx, rbx                  ; índice
 
+.read_loop:
+    lea rdx, [keyBuf]
+    call [rcx + EFI_SIMPLE_TEXT_INPUT_PROTOCOL.ReadKeyStroke]
+
+    mov ax, [keyBuf + 2]          ; UnicodeChar (UTF-16)
+    cmp ax, 0x0D                  ; Enter?
+    je .done
+
+    mov [rsi + rbx*2], ax         ; almacenar en inp
+    inc rbx
+    cmp rbx, 23
+    jb .read_loop
+    jmp .done
+
+.done:
+    mov word [rsi + rbx*2], 0     ; terminador UTF-16
+
+    ; --------------------------
+    ; mostrar "Terminamos"
+    mov rcx, [rdi + 0x40]            ; ConOut (offset 0x40)
+    lea rdx, [strEnd]
+    call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+
+    ; mostrar lo escrito por el usuario
+    lea rdx, [inp]
+    call [rcx + EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL.OutputString]
+
+    jmp .loopforever
+
+.loopforever:
+    hlt
+    jmp .loopforever
 codesize equ $ - $$
 
 section .reloc
 ; Solo para que UEFI lo encuentre
-
-section .data
-    strHello db __utf16__ `Hello World !\0`
-
-datasize equ $ - $$
